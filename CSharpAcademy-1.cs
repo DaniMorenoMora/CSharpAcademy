fb21dcfef1779beb4e9b8f8c8989d6c04dfdd6dc
DaniMoreno <danimorenomora@gmail.com>
2026-05-13 11:21:32 +0200

    Modulo 1 finalizado

--- CSharpAcademy-1.cs
+++ CSharpAcademy-1.cs
@@ -1,3 +1,2 @@
-// Console.WriteLine("Hello World");
-Console.WriteLine("Congratulations!");
-Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line");
+Console.Write("This is the second line");
