7c1b75aeba5ce1e07620dc40aaf67b402b1ebe6d
DaniMoreno <danimorenomora@gmail.com>
2026-05-14 11:39:21 +0200

    Modulo 4 completado

--- CSharpAcademy-4.cs
+++ CSharpAcademy-4.cs
@@ -1,3 +1,7 @@
+// Modulo 4
+
+//Ejercicio 1
+
 int sum = 7 + 5;
 int difference = 7 - 5;
 int product = 7 * 5;
@@ -11,3 +15,40 @@ Console.WriteLine("Difference: " + difference);
 Console.WriteLine("Product: " + product);
 Console.WriteLine("Quotient: " + quotient);
 Console.WriteLine("Decimal Quotient: " + decimaQuotient);
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+// Ejercicio 2
+
+int value = 1;
+
+value = value +1;   
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value --;
+Console.WriteLine("Third decrement: " + value);
+
+// Desafio 1
+
+int fahrenheit = 94;
+decimal celsiusConversor = (fahrenheit - 32) * (5m / 9m);
+decimal celsius = Math.Round(celsiusConversor,2);
+Console.WriteLine($"The temperature is {celsius} Celsius");
