3ae56c8f3b10d449488c055ea47fbde316e31565
DaniMoreno <danimorenomora@gmail.com>
2026-05-14 10:41:52 +0200

    Modulo 4, primer ejercicio de decimales

---
+++ CSharpAcademy-4.cs
@@ -0,0 +1,13 @@
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+decimal quotient = 7.00m / 5.00m;
+int first = 7;
+int second = 5;
+decimal decimaQuotient = (decimal)first / (decimal)second;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+Console.WriteLine("Decimal Quotient: " + decimaQuotient);
