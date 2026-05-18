8f158232bd1c363729c2cbad20e83818468e6720
DaniMoreno <danimorenomora@gmail.com>
2026-05-18 09:48:32 +0200

    Modulo 2, ejercicio de if y if else compleados

--- TestProject/UD2.cs
+++ TestProject/UD2.cs
@@ -1,8 +1,8 @@
-Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+
+int roll1 = 6;
+int roll2 = 6;
+int roll3 = 6;
 
 int total = roll1 + roll2 + roll3;
 
@@ -10,16 +10,32 @@ Console.WriteLine($"Tirada: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("¡Has sacado dobles! +2 de bonificación al total!");
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {    
+        Console.WriteLine("¡Has sacado triples! +6 de bonificación al total!");
+        total += 6; 
+    }
+
+    else
+    {
+        Console.WriteLine("¡Has sacado dobles! +2 de bonificación al total!");
     total += 2;
+    }
 }
 
-if (total >= 15)
+if (total >= 16)
 {
-    Console.WriteLine($"Has sacado {total} ¡Ganaste!");
+    Console.WriteLine($"Has sacado {total} ¡Has ganado un vehículo nuevo, yo ya no te veo en otro coche macho!");
 }
-
-if (total < 15)
+    else if (total >= 10)
+    {
+        Console.WriteLine ($"Has sacado {total} ¡Ganaste un portatil Lenovo guapisimo!");
+    }
+    else if (total >= 7)
+    {
+        Console.WriteLine($"Has sacado {total} ¡Ganaste un viaje a Kenia (Alicante)");
+    }
+else
 {
-    Console.WriteLine($"Has sacado {total} Lo siento, perdiste.");
+    Console.WriteLine($"Has sacado {total} ¡Has ganado un gatito gordito!");
 }
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4d614e1310b4cbcb1ed4a612fd80d100bf1c10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acdba4c8aad8e12c335434994e3529b8fbf4f9ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
