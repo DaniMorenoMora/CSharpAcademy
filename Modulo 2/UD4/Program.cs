b6e664184763adc12c2a72f758fd6d24cc63eb90
DaniMoreno <danimorenomora@gmail.com>
2026-05-19 08:14:49 +0200

    Modulo 2, Unidad 4 terimnada

---
+++ Modulo 2/UD4/Program.cs
@@ -0,0 +1,25 @@
+﻿string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+// Convert the message into a char array
+char[] message = originalMessage.ToCharArray();
+
+// Reverse the chars
+Array.Reverse(message);
+int letterCount = 0;
+
+// Count the o's
+foreach (char letter in message) 
+    {
+     if (letter == 'o') 
+        {
+         letterCount++; 
+         } 
+     }
+
+// Convert it back to a string
+string newMessage = new String(message);
+
+// Print it out
+Console.WriteLine(newMessage);
+
+Console.WriteLine($"'o' appears {letterCount} times.");
