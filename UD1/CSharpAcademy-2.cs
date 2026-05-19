829e507116f5028f199f61fa1b2a11b9717135fe
DaniMoreno <danimorenomora@gmail.com>
2026-05-19 08:19:42 +0200

    Cambio en la estructura de las carpetas y archivos

--- UD1/CSharpAcademy-2.cs
+++
@@ -1,7 +0,0 @@
-string firstName = "Bob";
-int numeroEntero = 3;
-float numeroDecimal = 34.4f;
-
-Console.Write("Hello, " + firstName + "! "
- + "You have " + numeroEntero + " messages in your inbox."
- + "The temperature is " + numeroDecimal + " celsius.");
--- UD3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿string [] pedidosIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-
-foreach (string pedidoId in pedidosIDs)
-{
-    if (pedidoId.StartsWith("B"))
-    {
-        Console.WriteLine($"{pedidoId}");
-    }
-}
